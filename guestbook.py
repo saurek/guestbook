8c3c5a69086838ecb64ae541f83e78e0ca8950dc
saurek <s.reklaitis@gmail.com>
2018-12-17 23:03:42 +0200

    02 - Endpoints

--- guestbook.py
+++ guestbook.py
@@ -8,5 +8,10 @@ def index():
     return '<h1>Hail Satan!</h1>'
 
 
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return '<h1>This is a home page</h1>'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
